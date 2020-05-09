167e7ebd32ec0b443c61c5ee8680b8ec68ae32a3
ivanpasev <ivanpasev@abv.bg>
2020-05-09 07:43:41 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,476 @@
+<!DOCTYPE html>
+<html lang="bg">
+<head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-165380395-1"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-165380395-1');
+	</script>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+	<meta name="robots" content="all,index,follow"/>
+	<meta name="msnbot" content="all,index,follow"/>
+	<meta name="description" content="Изработка на сайт и онлайн магазини. Перфектни цени и обслужване. Доверете се, за да получите безкомпромисно качество! " />
+	<meta name="keywords" content="Изработка на сайт, онлайн магазин, бургас, уеб дизайн, визуализации, изработка на уеб сайт, изработка на уеб дизайн" />
+    <meta property="og:title" content="Изработка на уеб сайт, SEO оптимизация, онлайн магазини и поддръжка" />
+	<meta property="og:description" content="Изработка на сайт и онлайн магазини. Перфектни цени и обслужване. Доверете се, за да получите безкомпромисно качество! " />
+	<meta property="og:type" content="article" />
+	<meta property="og:url" content="https://ivanpasev.website" />
+	<meta property="og:site_name" content="Web design" />
+	<meta property="og:image" content="https://ivanpasev.website/world.png" />
+	<link rel="canonical" href="https://ivanpasev.website" >
+	<title>Поръчайте Вашият уникален уеб сайт!</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link type="text/css" rel="stylesheet" href="css4.css">
+	<link rel="shortcut icon" type="image/ico" href="world.png" />
+	<link href="https://fonts.googleapis.com/css?family=Comfortaa&display=swap" rel="stylesheet">
+	<link href="http://bg.allfont.net/allfont.css?fonts=days" rel="stylesheet" type="text/css" />
+	<script src="//code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<script src="button.js"></script>
+	<script src="mobmenu.js"></script>
+</head>
+<body>
+	
+          
+          <section class="mobilemenu">
+            <section id="menuToggle">
+              <input type="checkbox" />
+                <span></span>
+                <span></span>
+                <span></span>
+            <ul id="menu">
+              <li><a class="mobilehref" href="#home">
+			  <img src="home.png" height="30">
+				&nbsp;НАЧАЛО</a></li>
+              <li><a class="mobilehref" href="#preferences">
+			  <img src="tools.png" height="30">
+				&nbsp;ИЗРАБОТКА</a></li>
+              <li><a class="mobilehref" href="#prices">
+			  <img src="price.png" height="30">
+				&nbsp;ЦЕНИ</a></li>
+              <li><a class="mobilehref" href="#contacts">
+			  <img src="contacts.png" height="30">
+				&nbsp;КОНТАКТИ</a></li>
+            </ul>
+           </section>
+          </section>
+       
+	<section id="fixed-header">
+		<a class="menu" href="#home">
+			<section class="button">
+				<img src="home.png" height="25">
+				&nbsp;НАЧАЛО
+			</section>
+		</a>
+		<a class="menu" href="#preferences">
+			<section class="button">
+				<img src="tools.png" height="25">
+				&nbsp;ИЗРАБОТКА
+			</section>
+		</a>
+		<a class="menu" href="#prices">
+			<section class="button">
+				<img src="price.png" height="25">
+				&nbsp;ЦЕНИ
+			</section>
+		</a>
+		<a class="menu" href="#contacts">
+			<section class="button">
+				<img src="contacts.png" height="25">
+				&nbsp;КОНТАКТИ
+			</section>
+		</a>
+	</section>
+	<!--MOBILNO MENU
+	<section class="container">
+      <section class="phone">-->
+        
+       <!--</section>
+    </section>-->
+
+	<section class="entry" id="home">
+	
+		<section class="centralbox">
+			<section class="title">
+				ИСКАТЕ УЕБСАЙТ
+			</section>
+			
+			
+			<br/>
+		<section class="textup">
+			Спрете да търсите! Дошли сте на правилното място. Аз ще Ви 
+			помогна да изградите Вашето успешно присъствие в Интернет. Без значение дали става въпрос за личен 
+			уебсайт, фирмен сайт - тип каталог или онлайн магазин, за мен ще е чест да съм част от Вашия
+			успех. Нека да Ви обясня малко по-подробно защо е важнo да имате онлайн присъствие и защо да изберете мен.</section>
+		</section>
+	</section>
+	<section class="preferences" id="preferences">
+		<span style='font-size: 35px; font-weight: bold;'></br>ЗАЩО Е ВАЖНО ДА ИМАТЕ УЕБСАЙТ?</span></br></br>
+		<section class ="preference1">
+			<img src="world.png" height="100" id='bottomMenu'></br>
+			<span style='font-weight: bold;'>ДОСТИГАТЕ ДО ПОВЕЧЕ ХОРА</span></br></br>
+			<section class ="preferencetext">
+				На практика няма ограничение в броят на хората, които могат да отворят сайта Ви и да се запознаят с Вашия бизнес или Вашите идеи.
+			</section>
+		</section>
+		<section class ="preference1">
+			<img src="nonstop.png" height="100" id='bottomMenu'></br><span style='font-weight: bold;'>РАБОТИ НОН СТОП</span>
+			</br>
+			</br>
+			<section class ="preferencetext">
+				Сайтът няма работно време. Дори и през нощта клиентите ще могат да направят поръчка или да резервират услуга. По този начин можете да увеличите продажбите си многократно.
+			</section>
+		</section>
+		<section class ="preference1">
+			<img src="marketing.png" height="100" id='bottomMenu'></br><span style='font-weight: bold;'>МАРКЕТИНГОВА МОЩ</span></br></br>
+			<section class ="preferencetext">
+				След като вече имате сайт, можете да добавите рекламна кампания и да увеличите клиентите си. Ако например обявите промоция чрез сайта ще привлечете нови клиенти, както и интереса на редовните. Можете да правите проучвания какво се харесва или пък не от страна на Вашите клиенти и така да предложите продукт с по-голям успех.
+				</br>
+				</br>
+			</section>
+		</section>
+		</br>
+			
+		<section class ="preference2">
+		<span style='position: relative; font-size: 35px; font-weight: bold;'>ЗАЩО ДА СЕ ДОВЕРИТЕ НА МЕН?</span>
+			</br>
+			</br>
+			<img src="trust.png" width="220" id='bottomMenu'>
+		</section>
+		</br>
+		<section class ="preference3">
+			Получавате изключително професионално и любезно отношение от моя страна. Изцяло съм ангажиран с Вашите изисквания и желания.
+		</section>
+		</br>
+		<section class ="preference2"><img src="responsive.png" width="220" id='bottomMenu'>
+		</section>
+		</br>
+		<section class ="preference3">
+			Получавате мобилна и стандартна версия на уебсайта като заплащате само за един сайт.</br>
+			Тъй като вече повече от 70 процента от потребителите отварят уебсайтовете чрез мобилния телефон, Вашият уебсайт ще изглежда по един начин на персонален компютър и лаптоп, но по друг начин на телефон. Пак ще е с подобен дизайн, но ще е приспособен
+			за по-малкият екран на телефона. По този начин придвижвате показванията на по добри места в търсачки като Google и ще имате повече и по доволни клиенти.
+		</section>
+		</br>
+		<section class ="preference2"><img src="ranking.png" width="220" id='bottomMenu'>
+		</section>
+		</br>
+		<section class ="preference3">
+			Сайтът Ви ще бъде оптимизиран за търсачки като Google
+			Имате всичко нужно за по-доброто представяне на Вашия уебсайт. Лек код, бързо отваряне, обработка на снимките, поставяне на ключови думи.
+		</section>
+		</br>
+		<section class ="preference2">
+			<img src="fast.png" width="220" id='bottomMenu'>
+		</section>
+		</br>
+		<section class ="preference3">
+			Бързи срокове на изработка.
+			В зависимост от сложността на изработка сайтът Ви ще бъде готов в рамките на 3 до 10 КАЛЕНДАРНИ дни.
+		</section>
+		</br>
+		</br>
+		</br>
+		<span style='font-size: 35px; font-weight: bold;'>
+			КАК ПРОТИЧА ПРОЦЕСА НА СЪЗДАВАНЕТО НА ВАШИЯ САЙТ?
+		</span>
+		</br>
+		</br>
+		<section class="etap">
+			<img src="info.png" height="150" id='bottomMenu'>
+			</br>
+			<span style='font-weight: bold;'>
+				ИНФОРМАЦИЯ
+			</span>
+			</br>
+			</br>
+			<section class ="etaptext">
+				Какъв уебсайт желаете. Може да ми изпратите чертеж като посочите какво искате да включва - продуктов каталог, поле с контактии т.н. Всички материалите за сайта може да изпращате чрез Viber или направо от тук.</br>
+				Също така трябва да изпратите текста, който да се публикува.
+			</section>
+		</section>
+		<section class="etap">
+			<img src="rocket.png" height="150" id='bottomMenu'>
+			</br>
+			<span style='font-weight: bold;'>
+				СТАРТИРАНЕ
+			</span>
+			</br>
+			</br>
+			<section class ="etaptext">
+				Не се притеснявайте ако текстът Ви затруднява. Все пак това си е професия - копирайтинг. Заедно ще обсъдим какво е най-добре за сайта и как най-добре ще се представи поекта Ви.</br>
+				Следващият етап е изработване на първоначална версия на сайта АБСОЛЮТНО БЕЗПЛАТНО.
+			</section>
+		</section>
+		<section class="etap">
+			<img src="net.png" height="150" id='bottomMenu'>
+			</br>
+			<span style='font-weight: bold;'>
+				ПРИКЛЮЧВАНЕ
+			</span>
+			</br>
+			</br>
+			<section class ="etaptext">
+				След като видите проекта ще ми кажете какво искате да променим, какво да добавим, така че да бъдете абсолютно удовлетворени. Заплащането става чрез банков превод и едва тогава, когато сте сигурни, че това е Вашият уебсайт.
+			</section>
+		</section>
+		<section class="etap">
+			<img src="monitoring.png" height="150" id='bottomMenu'>
+			</br>
+			<span style='font-weight: bold;'>
+				МОНИТОРИНГ
+			</span>
+			</br>
+			</br>
+			<section class ="etaptext">
+				В зависимост от проекта ще получите и кратко обучение как да оперирате с контролния панел, чрез който ще можете да добавяте страници, новини, снимки и др.
+				Можете да разчитате на моето съдействие и помощ и след пускането на уебсайта в Интернет.
+			</section>
+		</section>
+	</section>
+	<section class="prices" id="prices">
+		<span style='font-size: 35px; font-weight: bold;'>
+		</br>
+			КОЛКО ЩЕ ВИ СТРУВА?
+			</br>
+			</br>
+		</span>
+		<section class ="preference3">
+			Вашият уебсайт ще бъде направен специално за Вас, според Вашите нужди и желания. По този начин се гарантира, че няма да има никакви ограничения относно дизайна на сайта Ви.</br>
+			Посочените цени са ориентировъчни. Едва след като споделите с мен какво точно искате мога да Ви направя оферта. Разгледайте примерни планове:</br>
+		</section>
+		<section class ="preference1">
+			<img src="lowprice.png" height="100" id='bottomMenu'></br>
+			<span style='font-weight: bold;'>ВИЗИТКА - 450 лв.</span>
+			</br>
+			</br>
+			<section class ="preferencetext">
+				най-обикновен уеб сайт с няколко подстраници, например контакти, галерия. Можете да публикувате каталога на продукти.
+				Подходяш е за малки бизнес предприятия, хотели, спа центрове, малък магазин с няколко продукта.</br>
+			</section>
+		</section>
+		<section class ="preference1">
+			<img src="middleprice.png" height="100" id='bottomMenu'></br>
+			<span style='font-weight: bold;'>СРЕДНО СЛОЖЕН - 600 лв.</span>
+			</br>
+			</br>
+			<section class ="preferencetext">
+				Тук можем да добавим различни функции като продажба и приемане на поръчки чрез сайта, записване на часове за козметичен салон например, фирмен каталог на 50-100 артикула и описанието им (отнема време).</br>
+			</section>
+		</section>
+		<section class ="preference1">
+			<img src="highprice.png" height="100" id='bottomMenu'>
+			</br>
+			<span style='font-weight: bold;'>ОЩЕ ПО-СЛОЖЕН 800-1000 лв.</span></br></br>
+			<section class ="preferencetext">
+				Ако искате по сложен сайт, с множество изчисления, връзки към база данни и много подстраници това ще оскъпи сайта, единствено защото ще ми отнеме повече време, а както знаем то е най ценното. Споделете идеята си и ще Ви кажа конкретна цена.</br>
+				</br>
+				</br>
+			</section>
+		</section>
+		<section class ="preference3">
+			Тези цени са само за изработката. за да функционира Вашият уебсайт трябва да има също домейн - това е името на сайта, както и хостинг, които се плащат периодично.</br>
+			цени на домейни</br>
+			.com .net около 20 лева на година</br>
+			.bg около 80 лева на година</br>
+			Има и по евтини варианти ако не държите сайтът Ви да завършва на .com .net</br>
+			Хостнгът е мястото където физически се съхраняват данните на Вашия уебсайт. Цените варират, но ориентировъчно хостингът може да е примерно 60 лева за една година.</br>
+			Можете да закупите сами хостинг и домейн, на които да кача сайта Ви или ако не искате да се занимавате аз мога да направя това вместо Вас.</br>
+			ПОБЪРЗАЙТЕ! РЕАЛИЗИРАЙТЕ ВАШТЕ ИДЕИ И ПЕЧЕЛЕТЕ ПОВЕЧЕ ПАРИ!
+			</br></br></br>
+		</section>
+	</section>
+	<section id="contacts" class="contacts">
+		<section class="contacts1">
+						
+			<span style='font-size: 35px; font-weight: bold;'>
+			</br>
+				КОНТАКТИ
+				</br>
+				</br>
+			</span>
+			<section class="contactswindow">
+				<section class="hello" id="bottomMenu">
+					<span style="font-weight: bold; font-size: 22px;">
+						</br>
+						</br>
+							ЗДРАВЕЙТЕ!
+						</br>
+						</br>
+					</span>
+					Казвам се Иван и ще се радвам ако се свържите с мен, за да поговорим за Вашата идея.
+				</section>
+			</section>
+			<section class="contactswindow">
+				<section class="phone" id="bottomMenu">
+					</br>
+					</br>
+					<img src="contacts.png" height="50">
+					</br>
+					<span style='font-size: 31px; font-weight: bold;'>
+					<a style="color:black;" href="tel:+359895427789">089&nbsp;542&nbsp;77&nbsp;89</a>
+					</span>
+					</br>
+					<img src="mail.png" height="50">
+					</br>
+					<span style='font-size: 18px; font-weight: bold;'>
+					office@ivanpasev.website
+					</span>
+				</section>
+			</section>
+			<section class="contactswindow">
+				<section class="worktime" id="bottomMenu">
+					<span style="font-weight: bold; font-size: 22px;">
+						</br></br>РАБОТНО ВРЕМЕ</br><hr></br>
+					</span>
+					Понеделник - Петък:</br> 10 - 17 часа</br>
+					Събота:</br> 10 - 14 часа</br></br></br></br>
+				</section>
+			</section>
+
+			<section class="domaincheck"></br></br>
+				<p id="domaincheck">Как искате да се казва Вашият уебсайт?</br>
+				Проверете дали желания от Вас домейн е свободен.</p>
+					<form id="domain">
+					<input type="text" name="wname" id="wname" class="pole"><br><br> 
+					<input type="submit" name="submitwname" value="ПРОВЕРИ" id="submit1" class="buton">					
+				</form>
+				</br>
+				<section class="answercheck">
+				<section class="haho" style="display: none"></section>
+				</section>
+			</section>
+		</section>
+		<section class="feedback">
+			</br>
+			</br>
+			<span style='font-size: 35px; font-weight: bold;'>
+			</br>ФОРМА ЗА ОБРАТНА ВРЪЗКА</span>
+			<p>Моля попълнете формуляра, ако искате да се свържете.</p>
+			
+			<form id="contactForm">
+			<input type="text" placeholder="ИМЕ:" name="name" class="feedbackpole" id="name" required></br>
+			<section class="errorname" style="display: none"></section>
+			</br>
+			<input type="phone" placeholder="ТЕЛЕФОН:" name="phone" class="feedbackpole" id="phone" required></br>
+			<section class="erroremptyphone" style="display: none"></section>
+			<section class="errorinvalidphone" style="display: none"></section>
+			</br>
+			<input type="email" placeholder="ИМЕЙЛ:" name="email" class="feedbackpole" id="email" required></br>
+			<section class="erroremptymail" style="display: none"></section>
+			<section class="errorinvalidmail" style="display: none"></section>
+			</br>
+			<select id="budget" name="budget" class="feedbackpole">
+				<option value="choose">ИЗБЕРЕТЕ БЮДЖЕТ</option>
+				<option value="400">БЮДЖЕТ: до 400 лв.</option>
+				<option value="600">БЮДЖЕТ: между 400 и 800 лв.</option>
+				<option value="800">БЮДЖЕТ: между 800 и 1000 лв.</option>
+				<option value="1000plus">БЮДЖЕТ: над 1000 лв.</option>
+			</select></br></br>
+			<textarea placeholder="РАЗКАЖЕТЕ МИ ЗА ВАШИЯТ ПРОЕКТ:" name="message1" class="feedbacktextarea" id="message" required></textarea></br>
+			<section class="errormessage" style="display: none"></section>
+			<p>С натискане на "Изпрати" се съгласявам данните ми да бъдат обработени</p>
+			<button type="submit" class="submitbtn" id="submit">ИЗПРАТИ</button></br></br></br></br>
+			</form>
+			<section class="thanks" style="display: none">
+		</section>
+	</section>
+	<section class="infosajt">
+		<p style="text-align:center;">© 2020 Всички права запазени.</p><br />
+	</section>
+</body>
+<script type="text/javascript">
+  $(document).ready(function() {
+
+
+      $('#submit').click(function(e){
+        e.preventDefault();
+
+
+        var name = $("#name").val();
+		var phone = $("#phone").val();
+        var email = $("#email").val();
+        var budget = $("#budget").val();
+        var message1 = $("#message").val();
+
+
+        $.ajax({
+            type: "POST",
+            url: "/contactProcess.php",
+            dataType: "json",
+            data: {name:name, email:email, phone:phone, budget:budget, message1:message1},
+            success : function(data){
+                if (data.code == "200"){
+						$(".thanks").html("<ul>"+data.thanks+"</ul>");
+						$(".thanks").css("display","block");
+						$(".errorname").css("display","none");
+						$(".erroremptyphone").css("display","none");
+						$(".errorinvalidphone").css("display","none");
+						$(".erroremptymail").css("display","none");
+						$(".errorinvalidmail").css("display","none");
+						$(".errormessage").css("display","none");
+						$('#contactForm')[0].reset();
+                } else {
+						$(".errorname").html("<ul>"+data.error1+"</ul>");
+						$(".erroremptyphone").html("<ul>"+data.error2+"</ul>");
+						$(".errorinvalidphone").html("<ul>"+data.error3+"</ul>");
+						$(".erroremptymail").html("<ul>"+data.error4+"</ul>");
+						$(".errorinvalidmail").html("<ul>"+data.error5+"</ul>");
+						$(".errormessage").html("<ul>"+data.error6+"</ul>");
+						$(".errorname").css("display","block");
+						$(".erroremptyphone").css("display","block");
+						$(".errorinvalidphone").css("display","block");
+						$(".erroremptymail").css("display","block");
+						$(".errorinvalidmail").css("display","block");
+						$(".errormessage").css("display","block");
+						$(".thanks").css("display","none");
+                    //$(".display-error").html("<ul>"+data.msg+"</ul>");
+                    //$(".display-error").css("display","block");
+                }
+            }
+        });
+
+
+      });
+	  
+	  $('#submit1').click(function(e){
+        e.preventDefault();
+
+
+        var wname = $("#wname").val();
+		
+
+
+        $.ajax({
+            type: "POST",
+            url: "/checkname.php",
+            dataType: "json",
+            data: {wname:wname},
+			beforeSend:function()
+				{
+					$(".haho").html("<img src='wait.gif' height='50'>");
+						$(".haho").css("display","block");
+				},
+            success : function(data){
+                if (data.code == "200"){
+						$(".haho").html("<ul>"+data.haho+"</ul>");
+						$(".haho").css("display","block");
+						$('#domain')[0].reset();
+                } else {
+						$(".haho").css("display","none");
+                    //$(".display-error").html("<ul>"+data.msg+"</ul>");
+                    //$(".display-error").css("display","block");
+                }
+            }
+        });
+
+
+      });
+	  
+	  
+  });
+</script>
+</html>
